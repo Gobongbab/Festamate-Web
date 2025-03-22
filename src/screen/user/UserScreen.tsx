3781d90fcc8c754eea0f8de52554e553493da57d
cho4u4o <cho4u4o@gmail.com>
2025-03-23 02:26:05 +0900

    feat : implement `UserScreen`

--- src/screen/user/UserScreen.tsx
+++ src/screen/user/UserScreen.tsx
@@ -1,11 +1,14 @@
-import { AppScreen } from '@stackflow/plugin-basic-ui'
-import { Appbar, Dock } from '@/shared/ui'
+import { AppScreen } from '@stackflow/plugin-basic-ui';
+import { Appbar, Dock } from '@/shared/ui';
+import { UserContainer } from '@/widgets/user/ui';
 
 export default function UserScreen() {
   return (
     <AppScreen appBar={Appbar}>
-      <div className='scrollbar-hide container-mobile flex size-full flex-col gap-y-6 overflow-scroll overflow-y-scroll p-6 pb-19'></div>
+      <div className='scrollbar-hide container-mobile flex size-full flex-col gap-y-6 overflow-scroll overflow-y-scroll p-6 pb-19'>
+        <UserContainer />
+      </div>
       <Dock />
     </AppScreen>
-  )
+  );
 }
