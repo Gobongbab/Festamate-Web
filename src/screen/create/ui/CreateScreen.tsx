391871b14bbc7b7a1faf3b3e0a7024ddca02c225
cho4u4o <cho4u4o@gmail.com>
2025-03-26 10:09:40 +0900

    feat : change `AppBar` to function, add `NormalAppBar`

--- src/screen/create/ui/CreateScreen.tsx
+++ src/screen/create/ui/CreateScreen.tsx
@@ -2,16 +2,15 @@ import React from 'react';
 
 import { AppScreen } from '@stackflow/plugin-basic-ui';
 
-import { Appbar, Dock } from '@/shared/ui';
+import { NormalAppBar } from '@/shared/ui';
 import { CreateContainer } from '@/widgets/create/ui';
 
 export default function CreateScreen() {
   return (
-    <AppScreen appBar={Appbar}>
+    <AppScreen appBar={NormalAppBar('모임방 생성')}>
       <div className='scrollbar-hide container-mobile flex size-full flex-col gap-y-6 overflow-scroll overflow-y-scroll p-6 pb-20'>
         <CreateContainer />
       </div>
-      <Dock />
     </AppScreen>
   );
 }
--- src/screen/user/UserScreen.tsx
+++ src/screen/user/UserScreen.tsx
@@ -1,10 +1,10 @@
 import { AppScreen } from '@stackflow/plugin-basic-ui';
-import { Appbar, Dock } from '@/shared/ui';
+import { AppBar, Dock } from '@/shared/ui';
 import { UserContainer } from '@/widgets/user/ui';
 
 export default function UserScreen() {
   return (
-    <AppScreen appBar={Appbar}>
+    <AppScreen appBar={AppBar()}>
       <div className='scrollbar-hide container-mobile flex size-full flex-col gap-y-6 overflow-scroll overflow-y-scroll p-6 pb-19'>
         <UserContainer />
       </div>
