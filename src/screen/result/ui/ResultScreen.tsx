6d37547774a1ee562311165b50de13b137ceb4ae
cho4u4o <cho4u4o@gmail.com>
2025-03-27 16:23:10 +0900

    style : add `padding-bottom` due to dock bar

--- src/screen/result/ui/ResultScreen.tsx
+++ src/screen/result/ui/ResultScreen.tsx
@@ -13,7 +13,7 @@ export default function ResultScreen() {
 
   return (
     <AppScreen appBar={SearchAppBar(onClick)}>
-      <div className='scrollbar-hide container-mobile flex size-full flex-col gap-y-6 overflow-scroll overflow-y-scroll p-6'>
+      <div className='scrollbar-hide container-mobile flex size-full flex-col gap-y-6 overflow-scroll overflow-y-scroll p-6 pb-24'>
         <ResultContainer searchKey='과팅' />
       </div>
       <Dock />
