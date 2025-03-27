fa99cec3f83cb29aee81560b7134cb06aadfcfce
cho4u4o <cho4u4o@gmail.com>
2025-03-27 12:09:42 +0900

    feat : implement `SearchScreen`

--- src/screen/search/ui/SearchScreen.tsx
+++ src/screen/search/ui/SearchScreen.tsx
@@ -3,11 +3,14 @@ import React from 'react';
 import { AppScreen } from '@stackflow/plugin-basic-ui';
 
 import { SearchAppBar } from '@/shared/ui';
+import { SearchContainer } from '@/widgets/search/ui';
 
 export default function SearchScreen() {
   return (
     <AppScreen appBar={SearchAppBar()}>
-      <div className='scrollbar-hide container-mobile flex size-full flex-col gap-y-6 overflow-scroll overflow-y-scroll p-6'></div>
+      <div className='scrollbar-hide container-mobile flex size-full flex-col gap-y-6 overflow-scroll overflow-y-scroll p-6'>
+        <SearchContainer />
+      </div>
     </AppScreen>
   );
 }
