48a8806349f5c49d88946105f50fda00aaae7219
cho4u4o <cho4u4o@gmail.com>
2025-03-27 16:09:32 +0900

    refactor : change navigation method to `replace`

--- src/screen/search/ui/SearchScreen.tsx
+++ src/screen/search/ui/SearchScreen.tsx
@@ -4,10 +4,15 @@ import { AppScreen } from '@stackflow/plugin-basic-ui';
 
 import { SearchAppBar } from '@/shared/ui';
 import { SearchContainer } from '@/widgets/search/ui';
+import { useFlow } from '@/app/stackflow';
+import { PATH } from '@/shared/constants';
 
 export default function SearchScreen() {
+  const { replace } = useFlow();
+  const onClick = () => replace(PATH.RESULT, {}, { animate: false });
+
   return (
-    <AppScreen appBar={SearchAppBar()}>
+    <AppScreen appBar={SearchAppBar(onClick)}>
       <div className='scrollbar-hide container-mobile flex size-full flex-col gap-y-6 overflow-scroll overflow-y-scroll p-6'>
         <SearchContainer />
       </div>
