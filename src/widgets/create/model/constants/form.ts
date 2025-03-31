736e40ea00ee065bb3318a138b475e1e3ea4737a
cho4u4o <cho4u4o@gmail.com>
2025-04-01 02:18:36 +0900

    feat : separate constants of room creating form

---
+++ src/widgets/create/model/constants/form.ts
@@ -0,0 +1,11 @@
+export const DETAIL_OPTION = {
+  headCount: [
+    { id: 'people-2', value: '2', label: '2명 (1:1)' },
+    { id: 'people-4', value: '4', label: '4명 (2:2)' },
+    { id: 'people-6', value: '6', label: '6명 (3:3)' },
+  ],
+  preferredGender: [
+    { id: 'male', value: '남성', label: '남자' },
+    { id: 'female', value: '여성', label: '여자' },
+  ],
+};
---
+++ src/widgets/create/model/constants/room.ts
@@ -0,0 +1,2 @@
+export const TITLE_MAX_LENGTH = 10;
+export const CONTENT_MAX_LENGTH = 200;
