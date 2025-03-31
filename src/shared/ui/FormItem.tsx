d78a08b21216f8ebd2825ec284a892b3220e2644
cho4u4o <cho4u4o@gmail.com>
2025-04-01 02:16:34 +0900

    style : remove `childrenWrapper`

--- src/shared/ui/FormItem.tsx
+++ src/shared/ui/FormItem.tsx
@@ -11,7 +11,6 @@ export default function FormItem({
   title,
   description,
   children,
-  childrenWrapper = true,
 }: FormItemProps) {
   return (
     <div className='flex flex-col gap-y-3'>
@@ -19,13 +18,7 @@ export default function FormItem({
         <span className='title'>{title}</span>
         <span className='text-light'>{description}</span>
       </div>
-      {childrenWrapper ? (
-        <div className='bg-fill border-border rounded-5 flex w-fit gap-x-4 border-[1px] px-4 py-2'>
-          {children}
-        </div>
-      ) : (
-        <>{children}</>
-      )}
+      {children}
     </div>
   );
 }
