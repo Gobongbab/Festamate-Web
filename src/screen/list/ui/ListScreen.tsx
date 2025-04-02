61e3258a556a43ba839cc75b7f899304160640e1
cho4u4o <cho4u4o@gmail.com>
2025-04-02 22:44:46 +0900

    feat : add shared `Button` component

--- src/screen/list/ui/ListScreen.tsx
+++ src/screen/list/ui/ListScreen.tsx
@@ -14,7 +14,7 @@ const ListScreen: ActivityComponentType<ListScreenParams> = ({
 }) => {
   return (
     <AppScreen appBar={NormalAppBar(params.title)}>
-      <div className='scrollbar-hide container-mobile gap-y-normal-spacing p-normal-padding pb-dock-height flex size-full flex-col overflow-scroll overflow-y-scroll'>
+      <div className='scrollbar-hide container-mobile gap-y-normal-spacing p-normal-padding pb-dock-height flex size-full flex-col overflow-y-scroll'>
         <ListContainer />
       </div>
     </AppScreen>
