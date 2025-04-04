04ba5a49714b0645db07c85e6dcc084a69a5c53e
cho4u4o <cho4u4o@gmail.com>
2025-04-04 15:56:38 +0900

    feat : implement `LoginContainer` without API

---
+++ src/widgets/login/ui/LoginContainer.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { KakaoIcon } from '@/assets';
+import { Button } from '@/shared/ui';
+import { useFlow } from '@/app/stackflow';
+
+export default function LoginContainer() {
+  const { pop } = useFlow();
+  const handleClick = () => {
+    pop();
+  };
+
+  return (
+    <div className='grid size-full place-items-center'>
+      <div className='flex h-fit w-full flex-col items-center px-6'>
+        <p>축제를 더 즐겁게,</p>
+        <p className='logo agbalumo-regular mb-6'>Festamate!</p>
+        <Button
+          onClick={handleClick}
+          className='text-dark h-fit py-3 font-medium'
+          bgColor='bg-kakao'
+          label={
+            <div className='flex w-full items-center justify-center gap-x-4'>
+              <img src={KakaoIcon} className='size-6' />
+              <span>카카오톡 간편 로그인</span>
+            </div>
+          }
+          shadow={false}
+        />
+      </div>
+    </div>
+  );
+}
