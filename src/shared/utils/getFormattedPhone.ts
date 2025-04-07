31f3a97fb42c737268c53ae77e436511576f093c
cho4u4o <cho4u4o@gmail.com>
2025-04-07 18:04:22 +0900

    feat : add util formats phone number

---
+++ src/shared/utils/getFormattedPhone.ts
@@ -0,0 +1,12 @@
+export const getFormattedPhone = (value: string) => {
+  // 숫자만 남기기
+  const numbersOnly = value.replace(/\D/g, '');
+
+  if (numbersOnly.length <= 3) {
+    return numbersOnly;
+  } else if (numbersOnly.length <= 7) {
+    return `${numbersOnly.slice(0, 3)}-${numbersOnly.slice(3)}`;
+  } else {
+    return `${numbersOnly.slice(0, 3)}-${numbersOnly.slice(3, 7)}-${numbersOnly.slice(7, 11)}`;
+  }
+};
--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -1,3 +1,4 @@
 export * from './string';
 export * from './getDate';
 export * from './getCookie';
+export * from './getFormattedPhone';
