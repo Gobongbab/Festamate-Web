fb291badda080ed176b35479ad639305941af55e
cho4u4o <cho4u4o@gmail.com>
2025-04-07 18:05:23 +0900

    feat : change item of `DetailForm`

--- src/widgets/create/model/constants/form.ts
+++ src/widgets/create/model/constants/form.ts
@@ -3,7 +3,7 @@ export const DETAIL_OPTION = {
     { id: 'people-2', value: 2, label: '2명 (1:1)' },
     { id: 'people-4', value: 4, label: '4명 (2:2)' },
     { id: 'people-6', value: 6, label: '6명 (3:3)' },
-  ],
+  ] as const,
   preferredGender: [
     { id: 'male', value: '남성', label: '남자' },
     { id: 'female', value: '여성', label: '여자' },
--- src/widgets/create/ui/GroupDetailForm.tsx
+++ src/widgets/create/ui/GroupDetailForm.tsx
@@ -1,40 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { UseFormWatch, type UseFormRegister } from 'react-hook-form';
+import {
+  UseFormSetValue,
+  UseFormWatch,
+  type UseFormRegister,
+} from 'react-hook-form';
 
 import { FormItem, Input, Radio } from '@/shared/ui';
-import { Room } from '../types';
+import { Room } from '@/shared/types';
 import { DETAIL_OPTION } from '../model';
 import { getDate } from '@/shared/utils';
 
 interface GroupTitleFormProps {
   register: UseFormRegister<Room>;
   watch: UseFormWatch<Room>;
+  setValue: UseFormSetValue<Room>;
 }
 
 export default function GroupDetailForm({
   register,
   watch,
+  setValue,
 }: GroupTitleFormProps) {
-  const { openChatLink, preferredGender, headCount, meetingDateTime } = watch();
+  const { preferredGender, headCount, meetingDateTime } = watch();
+  const [headCountRender, setHeadCountRender] = useState(2);
 
   return (
     <>
-      <FormItem
-        title='인원 선택'
-        description='1:1은 2명, 2:2는 4명, 3:3은 6명을 선택해주세요.'
-      >
-        {DETAIL_OPTION.headCount.map(({ id, value, label }) => (
-          <Radio
-            key={id}
-            id={id}
-            value={value}
-            label={label}
-            checked={Number(headCount) === value}
-            {...register('headCount')}
-          />
-        ))}
-      </FormItem>
       <FormItem
         title='성별 선택'
         description='모임방에 들어올 구성원들의 성별을 선택해주세요.'
@@ -51,16 +43,38 @@ export default function GroupDetailForm({
         ))}
       </FormItem>
       <FormItem
-        title='연락 수단'
-        description='구성원들에게 연락할 수 있는 링크를 알려주세요.'
+        title='인원 선택'
+        description='1:1은 2명, 2:2는 4명, 3:3은 6명을 선택해주세요.'
       >
-        <Input
-          id='url'
-          value={openChatLink}
-          placeholder='오픈채팅 링크를 입력해주세요.'
-          {...register('openChatLink', { required: true })}
-        />
+        {DETAIL_OPTION.headCount.map(({ id, value, label }) => (
+          <Radio
+            key={id}
+            id={id}
+            value={value}
+            label={label}
+            checked={Number(headCount) === value}
+            onChange={() => {
+              setHeadCountRender(value);
+              setValue('headCount', value);
+            }}
+          />
+        ))}
       </FormItem>
+      {headCountRender !== 2 && (
+        <FormItem
+          title='친구'
+          description={
+            <>
+              <p>모임방을 함께 생성하는 친구의 전화번호를 입력해주세요.</p>
+              <p>친구도 Festamate!에 가입된 상태여야 해요!</p>
+            </>
+          }
+          className='flex flex-col gap-2'
+        >
+          <FriendInput />
+          {headCountRender === 6 && <FriendInput />}
+        </FormItem>
+      )}
       <FormItem
         title='모임 날짜'
         className='mb-normal-spacing flex gap-x-2'
@@ -84,3 +98,26 @@ export default function GroupDetailForm({
     </>
   );
 }
+
+const FriendInput = () => (
+  <div className='flex w-fit gap-2'>
+    <div className='w-50'>
+      <Input
+        id='co-founder'
+        placeholder='전화번호'
+        type='phone'
+        // onChange={e => {
+        //   const rawValue = e.target.value;
+        //   const formattedValue = getFormattedPhone(rawValue);
+        // }} 차후 해당 로직 연결 & 데이터 패칭 진행하면 됩니다.
+      />
+    </div>
+    <button
+      id='co-founder'
+      type='button'
+      className='bg-fill rounded-5 border-border hover:bg-sub cursor-pointer border-[1px] px-4 py-2'
+    >
+      친구 추가
+    </button>
+  </div>
+);
