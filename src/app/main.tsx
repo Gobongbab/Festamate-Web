be2e29a9a36ea98478104ad5f45c3235bd51646f
cho4u4o <cho4u4o@gmail.com>
2025-04-08 19:42:22 +0900

    feat : add router to handle login logic

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -5,11 +5,12 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { createRoot } from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import { Stack } from '@/app/stackflow';
 import { getToken } from 'firebase/messaging';
 import { messaging } from '@/app/fcm';
-import { post, REQUEST } from '@/shared/api';
+import { AuthScreen } from '@/screen/auth/ui';
 
 async function handleAllowNotification() {
   const permission = await Notification.requestPermission();
@@ -41,27 +42,22 @@ async function getDeviceToken() {
     });
 }
 
-async function getAccessToken() {
-  console.log('getAccessToken');
-  if (window.location.href.includes('/?code=')) {
-    const baseUrl = window.location.href;
-    const code = baseUrl.split('/?code=')[1];
-    const response = await post(REQUEST.LOGIN, { code: code });
-    console.log(response.data);
-    // window.location.replace('http://localhost:5173/');
-  }
-}
-
 handleAllowNotification();
 
-getAccessToken();
-
 const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Stack />,
+  },
+  { path: '/auth', element: <AuthScreen /> },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
     <RecoilRoot>
-      <Stack />
+      <RouterProvider router={router} />
     </RecoilRoot>
   </QueryClientProvider>,
 );
