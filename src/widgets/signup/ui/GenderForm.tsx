6fe1b37c7e8276893ba3c0ed675b5f4f6b6a27fe
cho4u4o <cho4u4o@gmail.com>
2025-04-08 22:50:27 +0900

    feat : implement `GenderForm` selects gender of user

---
+++ src/widgets/signup/ui/GenderForm.tsx
@@ -0,0 +1,44 @@
+import React, { useState, type Dispatch, type SetStateAction } from 'react';
+
+import { cn } from '@/shared/utils';
+
+interface GenderFormProps {
+  setProcess: Dispatch<SetStateAction<number>>;
+}
+
+export default function GenderForm({ setProcess }: GenderFormProps) {
+  const GENDER = ['여성', '남성'];
+  const [selected, setSelected] = useState<'여성' | '남성' | undefined>(
+    undefined,
+  );
+
+  const GenderButton = ({ label }: { label: string }) => (
+    <button
+      name='phone-number-auth'
+      className={cn(
+        'bg-fill border-border rounded-5 hover:bg-sub flex-1 cursor-pointer border-[1px] p-2 px-6 transition duration-150',
+        label === selected && 'bg-point/80 text-white',
+      )}
+      onClick={() => setSelected(label as '여성' | '남성')}
+    >
+      {label}
+    </button>
+  );
+
+  return (
+    <div className='flex w-full flex-col gap-6'>
+      <div className='flex w-60 items-center gap-2'>
+        {GENDER.map(gender => (
+          <GenderButton label={gender} key={gender} />
+        ))}
+      </div>
+      <button
+        name='phone-number-auth'
+        className='border-border w-fit cursor-pointer border-b-[1.5px] p-2 px-4'
+        onClick={() => setProcess(prev => prev + 1)}
+      >
+        다음
+      </button>
+    </div>
+  );
+}
