29e3e1da3afe5fc697ff22c60ab5c0197e6eec1d
cho4u4o <cho4u4o@gmail.com>
2025-04-09 10:26:11 +0900

    feat : add background image of `BoothInfo`

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,4 +1,5 @@
 import SignupSuccessBg from './signup-success.png';
 import StudentIdSample from './studentId-sample.png';
+import BoothInfoBg from './booth-info-bg.jpg';
 
-export { SignupSuccessBg, StudentIdSample };
+export { SignupSuccessBg, StudentIdSample, BoothInfoBg };
