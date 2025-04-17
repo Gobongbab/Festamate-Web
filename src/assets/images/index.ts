a92f7568f59ce5ce8ba6723cb938e0befc5c3088
cho4u4o <cho4u4o@gmail.com>
2025-04-17 19:48:20 +0900

    feat : add loader

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -2,5 +2,6 @@ import SignupSuccessBg from './signup-success.webp';
 import StudentIdSample from './student-id-sample.webp';
 import BoothInfoBg from './booth-info-bg.webp';
 import Error from './error.webp';
+import Loader from './loader.gif';
 
-export { SignupSuccessBg, StudentIdSample, BoothInfoBg, Error };
+export { SignupSuccessBg, StudentIdSample, BoothInfoBg, Error, Loader };
