c11b3ccc2579a967fa67bc76ffaed6bb0524747a
cho4u4o <cho4u4o@gmail.com>
2025-04-17 19:49:46 +0900

    feat : handle login and signup at `AuthPage`

--- src/screen/auth/api/auth.ts
+++ src/screen/auth/api/auth.ts
@@ -1,23 +1,49 @@
 import { post, REQUEST } from '@/shared/api';
 import { useMutation } from '@tanstack/react-query';
 
-interface PostKakaoRequest {
+interface KakaoTokenRequest {
   code: string;
 }
 
 interface KakaoTokenResponse {
-  access_token: string;
-  refresh_token: string;
+  isSuccess: boolean;
+  message: string;
+  result: {
+    kakaoAccessToken: string;
+    member: boolean;
+  };
 }
 
-const postKakaoToken = async (code: string): Promise<KakaoTokenResponse> => {
-  const response = await post<PostKakaoRequest>(REQUEST.LOGIN, { code: code });
-  return response.data.result;
+interface KakaoLoginRequest {
+  kakaoAccessToken: string;
+}
+
+const submitKakaoToken = async (code: string) => {
+  const response = await post<KakaoTokenRequest, KakaoTokenResponse>({
+    request: REQUEST.KAKAO,
+    data: { code: code },
+  });
+  return response.data;
+};
+
+const submitKakaoLogin = async (kakaoAccessToken: string) => {
+  const response = await post<KakaoLoginRequest>({
+    request: REQUEST.LOGIN,
+    data: { kakaoAccessToken: kakaoAccessToken },
+  });
+  console.log(response);
 };
 
 export const useKakaoToken = () => {
-  return useMutation({
-    mutationFn: postKakaoToken,
-    onSuccess: (data: KakaoTokenResponse) => data,
+  return useMutation<KakaoTokenResponse, unknown, { code: string }>({
+    mutationFn: ({ code }) => submitKakaoToken(code),
+    onSuccess: data => data,
+  });
+};
+
+export const useKakaoLogin = () => {
+  return useMutation<unknown, unknown, KakaoLoginRequest>({
+    mutationFn: ({ kakaoAccessToken }) => submitKakaoLogin(kakaoAccessToken),
+    onSuccess: data => data,
   });
 };
