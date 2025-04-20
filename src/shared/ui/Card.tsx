21f33f80c6ce7ccc442ca0f87700c41b6508d982
cho4u4o <cho4u4o@gmail.com>
2025-04-21 01:38:46 +0900

    feat: add icons and improve layout in Card and ListItem components

--- src/shared/ui/Card.tsx
+++ src/shared/ui/Card.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 import { RiUser3Fill } from 'react-icons/ri';
 import { IoMdFemale, IoMdMale } from 'react-icons/io';
+import { IoLocationSharp } from 'react-icons/io5';
+import { GoClockFill } from 'react-icons/go';
 
 import { useFlow } from '@/app/stackflow';
 import { PATH } from '@/shared/constants';
 import { RoomListItem } from '@/shared/types';
 import { cn, getDate } from '@/shared/utils';
-import { IoLocationSharp } from 'react-icons/io5';
 
 export default function Card(props: RoomListItem) {
   const { push } = useFlow();
@@ -37,11 +38,7 @@ export default function Card(props: RoomListItem) {
       onClick={handleClick}
     >
       <div className='rounded-10 absolute inset-0 z-0 bg-black/40 bg-gradient-to-b from-transparent from-0% via-transparent via-50% to-black/60 to-100%' />
-      <div className='absolute top-0 z-10 flex w-full flex-col items-start gap-1.5 p-4 text-sm text-white'>
-        <div className='flex items-center gap-x-1 rounded-full border-[0.5px] border-white bg-white/30 px-2 py-1'>
-          <RiUser3Fill size={12} /> {currentParticipants}명 / {maxParticipants}
-          명
-        </div>
+      <div className='absolute top-0 z-10 flex w-full flex-col items-start gap-1.5 p-2 text-sm text-white'>
         <div
           className={cn(
             'flex items-center gap-x-1 rounded-full border-[0.5px] px-2 py-1',
@@ -60,13 +57,24 @@ export default function Card(props: RoomListItem) {
         </div>
       </div>
       <div className='card-body z-10 justify-end p-4 text-white'>
-        <div className='flex flex-col items-start gap-y-1 text-start'>
-          <p className='text w-[80%] text-lg leading-tight font-semibold text-wrap'>
+        <div className='flex flex-col items-start gap-y-1 overflow-hidden text-start'>
+          <p className='w-36 overflow-hidden text-lg font-semibold text-nowrap text-ellipsis whitespace-nowrap'>
             {title}
           </p>
           <div className='text-sub flex flex-col overflow-hidden text-sm'>
-            <span>{getDate(meetingDateTime, 'ddd요일 A h시 m분')}</span>
-            <p className='flex items-center justify-start gap-x-1 overflow-hidden'>
+            <p className='flex items-center justify-start gap-x-1.5 overflow-hidden'>
+              <RiUser3Fill size={12} />
+              <span className='w-32 overflow-hidden text-start text-nowrap text-ellipsis'>
+                {currentParticipants}명 / {maxParticipants}명
+              </span>
+            </p>
+            <p className='flex items-center justify-start gap-x-1.5 overflow-hidden'>
+              <GoClockFill size={11} />
+              <span className='w-32 overflow-hidden text-start text-nowrap text-ellipsis'>
+                {getDate(meetingDateTime, 'ddd요일 A h:mm')}
+              </span>
+            </p>
+            <p className='flex items-center justify-start gap-x-1.5 overflow-hidden'>
               <IoLocationSharp size={12} />
               <span className='w-32 overflow-hidden text-start text-nowrap text-ellipsis'>
                 {place}
