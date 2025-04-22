c37a3c571002fab24fd20cad05a5f95d7e515dc1
cho4u4o <cho4u4o@gmail.com>
2025-04-22 19:39:49 +0900

    feat: update `RoomScreen` and modals to handle room participation with friends

--- src/screen/room/ui/RoomScreen.tsx
+++ src/screen/room/ui/RoomScreen.tsx
@@ -1,6 +1,6 @@
 import { ActivityComponentType } from '@stackflow/react';
 import { AppScreen } from '@stackflow/plugin-basic-ui';
-import { RoomAppBar } from '@/shared/ui';
+import { Button, RoomAppBar } from '@/shared/ui';
 import {
   MenuBottomSheet,
   RoomContainer,
@@ -8,8 +8,9 @@ import {
   RoomJoinModal,
 } from '@/widgets/room/ui';
 import { RoomListItem } from '@/shared/types';
-import { useBottomSheet } from '@/shared/hook';
-import { BOTTOM_SHEET } from '@/shared/constants';
+import { useBottomSheet, useModal } from '@/shared/hook';
+import { BOTTOM_SHEET, MODAL } from '@/shared/constants';
+import { fetchLoginStatus } from '@/shared/utils';
 
 const RoomScreen: ActivityComponentType<RoomListItem> = ({
   params,
@@ -17,7 +18,23 @@ const RoomScreen: ActivityComponentType<RoomListItem> = ({
   params: RoomListItem;
 }) => {
   const { openBottomSheet } = useBottomSheet();
+  const { openModal } = useModal();
+  const { maxParticipants, currentParticipants } = params;
+
+  const isLogin = fetchLoginStatus();
+  const availableFriendCnt = maxParticipants - currentParticipants - 1;
+  const isAvailableWithFriend = maxParticipants - currentParticipants - 1 >= 1;
+
   const handleMenuClick = () => openBottomSheet(BOTTOM_SHEET.MENU);
+
+  const handleJoin = () => {
+    if (isLogin) openModal(MODAL.JOIN);
+    else openBottomSheet(BOTTOM_SHEET.LOGIN);
+  };
+  const handleJoinWithFriend = () => {
+    if (isLogin) openModal(MODAL.JOIN_WITH_FRIEND);
+    else openBottomSheet(BOTTOM_SHEET.LOGIN);
+  };
 
   return (
     <>
@@ -26,9 +43,27 @@ const RoomScreen: ActivityComponentType<RoomListItem> = ({
           <RoomContainer {...params} />
         </div>
       </AppScreen>
+      <div className='border-t-app-bar-border fixed bottom-0 z-30 flex h-fit w-full gap-x-3 border-[0.5px] bg-white px-6 py-6 text-lg font-semibold text-white'>
+        <Button
+          name='room-participate'
+          label='참여하기'
+          halfWidth={isAvailableWithFriend}
+          onClick={handleJoin}
+          className='mb-normal-spacing'
+        />
+        {isAvailableWithFriend && (
+          <Button
+            name='room-participate-with-friend'
+            label='친구와 함께 참여하기'
+            halfWidth
+            onClick={handleJoinWithFriend}
+            className='mb-normal-spacing'
+          />
+        )}
+      </div>
       <MenuBottomSheet />
       <RoomJoinModal />
-      <RoomJoinFriendModal />
+      <RoomJoinFriendModal availableFriendCnt={availableFriendCnt} />
     </>
   );
 };
--- src/widgets/room/ui/RoomJoinModal.tsx
+++ src/widgets/room/ui/RoomJoinModal.tsx
@@ -5,28 +5,33 @@ import { Button, Modal } from '@/shared/ui';
 import { useModal } from '@/shared/hook';
 
 export default function RoomJoinModal() {
-  const { isOpen } = useModal().modalState(MODAL.JOIN);
+  const { closeModal, modalState } = useModal();
+  const { isOpen } = modalState(MODAL.JOIN);
+  const onClose = () => closeModal(MODAL.JOIN);
 
   return (
     <>
       {isOpen && (
         <Modal modalKey={MODAL.JOIN}>
-          <p className='text-lg font-semibold'>모임방 가입하기</p>
-          <p>티켓 한 장을 소모하고 모임방에 가입할까요?</p>
+          <p className='text-lg font-semibold'>모임방 참여하기</p>
+          <p>티켓 한 장을 소모하고 모임방에 참여할까요?</p>
           <div className='mt-2 flex gap-3'>
             <Button
               halfWidth
               noMargins
+              name='joinWithdraw'
               label='더 생각해 볼래요'
               className='bg-border text-dark m-0'
-              size='lg'
+              size='md'
+              onClick={onClose}
             />
             <Button
               halfWidth
               noMargins
-              label='가입할래요'
+              name='joinConfirm'
+              label='참여할래요'
               className='m-0'
-              size='lg'
+              size='md'
             />
           </div>
         </Modal>
