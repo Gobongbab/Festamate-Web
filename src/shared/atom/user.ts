25b4f926f9bff789edeeb5d77a302a2d032e16a2
cho4u4o <cho4u4o@gmail.com>
2025-04-23 18:28:17 +0900

    feat : add `userAtom` stores user information

--- src/shared/atom/user.ts
+++ src/shared/atom/user.ts
@@ -1,5 +1,7 @@
+import { atom } from 'jotai';
 import { atomWithStorage, createJSONStorage } from 'jotai/utils';
 import { AsyncStorage } from 'jotai/vanilla/utils/atomWithStorage';
+import { User } from '@/shared/types';
 
 export const KakaoAccessTokenAtom = atomWithStorage('kakaoAccessToken', {
   kakaoAccessToken: '',
@@ -22,3 +24,5 @@ export const userTokenAtom = atomWithStorage<Token>(
   initialUserState,
   sessionStorage as AsyncStorage<Token>,
 );
+
+export const userAtom = atom<User | null>(null);
