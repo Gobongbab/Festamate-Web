a3cc90967fdf34a35736c09d24333f5019e3c56b
cho4u4o <cho4u4o@gmail.com>
2025-04-25 14:36:49 +0900

    remove : delete unused property

--- src/widgets/search/ui/SearchContainer.tsx
+++ src/widgets/search/ui/SearchContainer.tsx
@@ -6,12 +6,12 @@ import { POPULAR_KEYWORDS } from '@/widgets/search/model';
 export default function SearchContainer() {
   return (
     <>
-      <FormItem title='최근 검색어' childrenWrapper={false}>
+      <FormItem title='최근 검색어'>
         <div className='grid h-40 w-full place-items-center'>
           최근 검색어가 없습니다.
         </div>
       </FormItem>
-      <FormItem title='인기 검색어' childrenWrapper={false}>
+      <FormItem title='인기 검색어'>
         <div className='flex h-fit w-full flex-wrap gap-2'>
           {POPULAR_KEYWORDS.map(keyword => (
             <span
