95a14fcbb4190deea21ad254527931d0b690c3ad
cho4u4o <cho4u4o@gmail.com>
2025-04-27 23:34:02 +0900

    style : change `BoothInfo` background image

--- src/widgets/home/ui/HomeContainer.tsx
+++ src/widgets/home/ui/HomeContainer.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { GroupCarousel, GroupList } from '@/shared/ui';
 import { PATH } from '@/shared/constants';
 import { REQUEST } from '@/shared/api';
-import { BoothInfoBg } from '@/assets/images';
 import { fetchLoginStatus } from '@/shared/utils';
 import { useAtomValue } from 'jotai';
 import { userAtom } from '@/shared/atom';
@@ -39,7 +38,9 @@ export default function HomeContainer() {
 const BoothInfo = () => (
   <div
     className='rounded-10 p-normal-spacing relative flex h-fit w-full flex-col gap-y-2 bg-cover bg-center text-white'
-    style={{ backgroundImage: `url(${BoothInfoBg})` }}
+    style={{
+      backgroundImage: `url('https://i.pinimg.com/736x/67/a7/a2/67a7a2316cfa18d84af9f03cf11b0919.jpg')`,
+    }}
   >
     <div className='rounded-10 absolute inset-0 bg-black/30' />
     <div className='z-1 flex items-baseline justify-between'>
@@ -48,6 +49,6 @@ const BoothInfo = () => (
         <u>위치보기</u>
       </span>
     </div>
-    <span className='z-1 text-xl font-semibold'>부스에 방문해보세요!</span>
+    <span className='z-1 text-lg font-semibold'>부스에 방문해보세요!</span>
   </div>
 );
