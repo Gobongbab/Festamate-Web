3796b57fba1926d2338db441f13d0026c7cbec06
cho4u4o <cho4u4o@gmail.com>
2025-04-27 18:38:12 +0900

    remove : delete unused property and imports

--- src/shared/ui/ListItem.tsx
+++ src/shared/ui/ListItem.tsx
@@ -1,13 +1,11 @@
 import React, { forwardRef } from 'react';
 
 import { RiUser3Fill } from 'react-icons/ri';
-import { IoLocationSharp } from 'react-icons/io5';
-import { GoClockFill } from 'react-icons/go';
 
 import { useFlow } from '@/app/stackflow';
 import { GENDER, PATH } from '@/shared/constants';
 import { RoomListItem } from '@/shared/types';
-import { cn, getDate } from '@/shared/utils';
+import { cn } from '@/shared/utils';
 
 interface ListItemProps {
   header?: boolean;
@@ -21,10 +19,7 @@ const ListItem = forwardRef<HTMLButtonElement, RoomListItem & ListItemProps>(
       thumbnail,
       maxParticipants,
       currentParticipants,
-      meetingDateTime,
       preferredGender,
-      place,
-      header = false,
     } = props;
     const handleClick = () => {
       push(PATH.ROOM, { ...props });
@@ -35,11 +30,9 @@ const ListItem = forwardRef<HTMLButtonElement, RoomListItem & ListItemProps>(
         name='listitem'
         className={cn(
           'rounded-10 grid h-30 w-full cursor-pointer grid-cols-[1fr_3.5fr] gap-3 focus:outline-none',
-          header ? 'pb-3' : 'py-3',
         )}
         onClick={handleClick}
         ref={ref}
-        disabled={header}
       >
         <div
           className='rounded-5 bg-fill h-full w-24 bg-cover bg-center'
@@ -48,26 +41,10 @@ const ListItem = forwardRef<HTMLButtonElement, RoomListItem & ListItemProps>(
         <div className='flex h-full flex-col justify-center gap-y-2 overflow-hidden'>
           <div className='flex flex-col items-start text-lg'>
             <p className='font-semibold'>{title}</p>
-            {!header && (
-              <p className='text-md w-full overflow-hidden text-start text-nowrap text-ellipsis'>
-                {getDate(meetingDateTime, 'M월 DD일 ddd요일 A h시 mm분')}
-              </p>
-            )}
           </div>
           <div className='flex flex-col gap-1'>
             <div className='text-light flex w-full items-center justify-between'>
               <div className='rounded-5 text-light flex w-fit items-center gap-x-1.5 text-sm'>
-                {!header && (
-                  <>
-                    <p className='flex items-center justify-start gap-x-1 overflow-hidden'>
-                      <IoLocationSharp size={12} />
-                      <span className='w-fit max-w-14 overflow-hidden text-start text-nowrap text-ellipsis'>
-                        {place}
-                      </span>
-                    </p>
-                    ·
-                  </>
-                )}
                 <p className='flex items-center gap-x-1'>
                   <RiUser3Fill size={12} /> {currentParticipants}명 /{' '}
                   {maxParticipants}명
@@ -83,20 +60,6 @@ const ListItem = forwardRef<HTMLButtonElement, RoomListItem & ListItemProps>(
                 </p>
               </div>
             </div>
-            {header && (
-              <>
-                <div className='text-light flex items-center gap-1.5 text-start text-sm'>
-                  <GoClockFill size={11} />
-                  <span className='w-32 overflow-hidden text-start text-nowrap text-ellipsis'>
-                    {getDate(meetingDateTime, 'ddd요일 A h:mm')}
-                  </span>
-                </div>
-                <div className='text-light flex items-center gap-1.5 text-start text-sm'>
-                  <IoLocationSharp size={12} />
-                  {place}
-                </div>
-              </>
-            )}
           </div>
         </div>
       </button>
