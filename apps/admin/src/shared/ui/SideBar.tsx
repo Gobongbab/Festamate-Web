cc6ad85aade0d45143648056b5d99280418b623d
cho4u4o <cho4u4o@gmail.com>
2025-04-30 16:21:10 +0900

    feat(admin): add logout functionality

--- apps/admin/src/shared/ui/SideBar.tsx
+++ apps/admin/src/shared/ui/SideBar.tsx
@@ -4,6 +4,11 @@ import { PATH } from '../constants';
 import { cn } from '@festamate/utils';
 
 export default function SideBar() {
+  const handleLogoutClick = () => {
+    sessionStorage.removeItem('userToken');
+    window.location.reload();
+  };
+
   return (
     <div className='box-shadow-dock border-border flex h-screen w-56 flex-col gap-20 border-r-[0.5px] px-6 py-12'>
       <p className='agbalumo-regular logo text-center'>Festamate!</p>
@@ -12,6 +17,13 @@ export default function SideBar() {
         <SideBarButton to={PATH.BLOCK} label='신고 조회' />
         <SideBarButton to={PATH.ROOM} label='모임 조회' />
       </div>
+      <button
+        name='logout'
+        className='cursor-pointer text-lg focus:outline-none'
+        onClick={handleLogoutClick}
+      >
+        로그아웃
+      </button>
     </div>
   );
 }
@@ -22,7 +34,7 @@ const SideBarButton = ({ label, to }: { to: string; label: string }) => {
       to={to}
       className={({ isActive }) =>
         cn(
-          'hover:text-point border-point/60 w-fit cursor-pointer p-2 py-3 transition duration-150 hover:border-b-2 focus:outline-none',
+          'hover:text-point border-point/60 w-fit cursor-pointer p-2 py-3 text-xl transition duration-150 hover:border-b-2 focus:outline-none',
           isActive && 'text-point border-b-2',
         )
       }
