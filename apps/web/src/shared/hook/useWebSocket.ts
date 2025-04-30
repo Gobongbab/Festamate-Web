53810cce27481296fa1804d5dce3bb766ee0dfda
cho4u4o <cho4u4o@gmail.com>
2025-04-30 17:16:04 +0900

    fix(web): change the way socket connects

--- apps/web/src/shared/hook/useWebSocket.ts
+++ apps/web/src/shared/hook/useWebSocket.ts
@@ -7,13 +7,23 @@ interface UseWebSocketProps {
   onMessage: (message: string) => void;
 }
 
+interface Subscription {
+  unsubscribe: () => void;
+}
+
 export default function useWebSocket({
   chatRoomId,
   onMessage,
 }: UseWebSocketProps) {
   const client = useRef<Client | null>(null);
+  const subscriptions = useRef<{ [key: string]: Subscription }>({});
 
   const connect = useCallback(() => {
+    if (client.current?.connected) {
+      console.log('이미 연결된 상태입니다.');
+      return;
+    }
+
     const socket = new SockJS('https://www.festamate.shop/ws');
     const stompClient = Stomp.over(socket);
 
@@ -23,10 +33,14 @@ export default function useWebSocket({
         console.log('WebSocket 연결 성공!');
         client.current = stompClient;
 
-        stompClient.subscribe(`/topic/chatRooms/${chatRoomId}`, message => {
+        const path = `/topic/chatRooms/${chatRoomId}`;
+        const subscription = stompClient.subscribe(path, message => {
+          console.log('메시지 수신:', message);
           const receivedMessage = JSON.parse(message.body);
           onMessage(receivedMessage.message);
         });
+
+        subscriptions.current[path] = subscription;
       },
       (error: Error) => {
         console.error('WebSocket 연결 실패:', error);
@@ -37,6 +51,13 @@ export default function useWebSocket({
   const disconnect = useCallback(() => {
     if (client.current) {
       console.log('WebSocket 연결을 종료합니다.');
+
+      // 모든 구독 해제
+      Object.keys(subscriptions.current).forEach(path => {
+        subscriptions.current[path].unsubscribe();
+        delete subscriptions.current[path];
+      });
+
       client.current.deactivate();
       client.current = null;
     }
@@ -44,19 +65,20 @@ export default function useWebSocket({
 
   const sendMessage = useCallback(
     (message: string) => {
-      if (client.current?.connected) {
-        try {
-          client.current.publish({
-            destination: `/topic/chatRooms/${chatRoomId}`,
-            body: JSON.stringify({ message }),
-          });
-        } catch (error) {
-          console.error('메시지 전송 중 에러 발생:', error);
-        }
-      } else {
-        console.warn(
-          'WebSocket이 연결되지 않았습니다. 메시지를 전송할 수 없습니다.',
-        );
+      if (!client.current?.connected) {
+        console.warn('WebSocket이 연결되지 않았습니다.');
+        return;
+      }
+
+      try {
+        const destination = `/app/chat/room/${chatRoomId}`;
+        client.current.publish({
+          destination,
+          body: JSON.stringify({ message }),
+        });
+        console.log('메시지 전송 성공:', message);
+      } catch (error) {
+        console.error('메시지 전송 중 에러 발생:', error);
       }
     },
     [chatRoomId],
