9865cf867c7a677371641a8d062062a3e0b0615f
cho4u4o <cho4u4o@gmail.com>
2025-05-01 10:57:24 +0900

    feat(admin): add loader function and fallback UI

---
+++ apps/admin/src/shared/ui/Loader.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Loader() {
+  return (
+    <div className='grid h-screen w-full place-items-center'>
+      <span className='text-light text-lg'>데이터를 가져오는 중..</span>
+    </div>
+  );
+}
--- apps/admin/src/shared/ui/index.ts
+++ apps/admin/src/shared/ui/index.ts
@@ -1,3 +1,4 @@
-export { default as SideBar } from './SideBar';
 export { default as Input } from './Input';
+export { default as Loader } from './Loader';
 export { default as PageHeader } from './PageHeader';
+export { default as SideBar } from './SideBar';
