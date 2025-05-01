30c3e82196c86e308109385841b646df1c34c103
cho4u4o <cho4u4o@gmail.com>
2025-05-01 09:54:57 +0900

    refactor(admin): refactor to use shared component

--- apps/admin/src/pages/user/ui/UserPage.tsx
+++ apps/admin/src/pages/user/ui/UserPage.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 
 import { UserSearchBoard } from '@/features/user/ui';
+import { PageHeader } from '@/shared/ui';
 
 export default function UserPage() {
   return (
     <div className='flex flex-col gap-6'>
-      <div className='flex flex-col gap-1'>
-        <p className='text-xl font-semibold'>회원 조회하기</p>
-        <p className='text-lg'>
-          상세 정보를 확인하고자 하는 회원의 ID로 회원을 조회해요.
-        </p>
-      </div>
+      <PageHeader
+        title='회원 조회하기'
+        description='상세 정보를 확인하고자 하는 회원의 ID로 회원을 조회해요.'
+      />
       <UserSearchBoard />
     </div>
   );
