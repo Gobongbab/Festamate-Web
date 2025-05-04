24605d9b792452faf7083f603b4cb1f7660f6307
cho4u4o <cho4u4o@gmail.com>
2025-05-04 23:53:44 +0900

    feat(web): implement `SearchAppBar` and apply search functionality to result / search screen

--- apps/web/src/screen/result/ui/ResultScreen.tsx
+++ apps/web/src/screen/result/ui/ResultScreen.tsx
@@ -1,28 +1,46 @@
-import React from 'react';
+import React, { type FormEvent } from 'react';
+import { useAtom } from 'jotai';
 
 import { AppScreen } from '@stackflow/plugin-basic-ui';
+import { type ActivityComponentType } from '@stackflow/react';
 import { MdAdd } from 'react-icons/md';
 
 import { useFlow } from '@/app/stackflow';
 import { PATH } from '@/shared/constants';
+import { searchAtom } from '@/shared/atom';
 import { SearchAppBar, Dock } from '@/shared/ui';
+
+import { useRecentSearches } from '@/widgets/search/model';
 import { ResultContainer } from '@/widgets/result/ui';
 
-export default function ResultScreen() {
+const ResultScreen: ActivityComponentType<{ searchKey: string }> = ({
+  params,
+}: {
+  params: { searchKey: string };
+}) => {
+  const [value, setValue] = useAtom(searchAtom);
+  const { addSearch } = useRecentSearches();
   const { replace, push } = useFlow();
+
   const closeOnClick = () => replace(PATH.HOME, {});
-  const searchOnClick = () => replace(PATH.RESULT, {}, { animate: false });
+  const searchOnClick = (e: FormEvent) => {
+    e.preventDefault();
+    addSearch(value);
+    replace(PATH.RESULT, { searchKey: value }, { animate: false });
+  };
   const createOnClick = () => push(PATH.CREATE, {});
 
   return (
     <div className='fixed inset-0 overflow-hidden'>
-      <AppScreen appBar={SearchAppBar(closeOnClick, searchOnClick)}>
+      <AppScreen
+        appBar={SearchAppBar(closeOnClick, searchOnClick, value, setValue)}
+      >
         <div className='scrollbar-hide container-mobile gap-y-normal-spacing p-normal-padding flex size-full flex-col overflow-scroll overflow-y-scroll pb-24'>
-          <ResultContainer searchKey='과팅' />
+          <ResultContainer searchKey={params.searchKey} />
         </div>
         <button
           name='create-group'
-          className='box-shadow-buttonLg hover:bg-primary-hover absolute right-6 bottom-26 z-30 flex w-fit flex-shrink-0 cursor-pointer items-center gap-x-2 rounded-full bg-[#775bf0] px-5 py-2.5 text-lg font-medium text-white'
+          className='box-shadow-buttonLg hover:bg-primary-hover absolute right-6 bottom-18 z-30 flex w-fit flex-shrink-0 cursor-pointer items-center gap-x-2 rounded-full bg-[#775bf0] px-5 py-2.5 text-lg font-medium text-white'
           onClick={createOnClick}
         >
           <MdAdd size={14} /> 모임 만들기
@@ -31,4 +49,6 @@ export default function ResultScreen() {
       </AppScreen>
     </div>
   );
-}
+};
+
+export default ResultScreen;
