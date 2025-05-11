04c6c7d5ea11fa8da5b0712812a8b9ee68452759
cho4u4o <cho4u4o@gmail.com>
2025-05-11 19:18:33 +0900

    design(web): improve modal text line visibility

--- apps/web/src/widgets/home/ui/TicketInfoModal.tsx
+++ apps/web/src/widgets/home/ui/TicketInfoModal.tsx
@@ -15,12 +15,23 @@ export default function TicketInfoModal() {
         <Modal modalKey={MODAL.TICKET_INFO}>
           <>
             <p className='text-lg font-semibold'>Festamate! 티켓이란?</p>
-            <p>
-              Festamate! 티켓은 모임을 개설하거나 참여할 때마다 하나씩 소모돼요.
-              모임에 참여했더라도, 모임이 취소된 경우에는 티켓을 돌려받을 수
-              있어요. 더 많은 티켓을 사용하고 싶다면, Festamate! 부스로
-              찾아오세요!
-            </p>
+            <div className='flex flex-col gap-2'>
+              <p>
+                Festamate! 티켓은 모임을 개설하거나
+                <br />
+                참여할 때마다 하나씩 소모돼요.
+              </p>
+              <p>
+                모임에 참여했더라도, 모임이 취소된 경우에는
+                <br />
+                티켓을 돌려받을 수 있어요.
+              </p>
+              <p>
+                더 많은 티켓을 사용하고 싶다면,
+                <br />
+                Festamate! 부스로 찾아오세요!
+              </p>
+            </div>
             <div className='mt-2 flex gap-3'>
               <Button
                 name='deleteWithdraw'
