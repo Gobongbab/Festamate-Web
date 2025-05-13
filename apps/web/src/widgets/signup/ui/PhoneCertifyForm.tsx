1f943c25b3555f66ea4bbda9b98e21b50e3f3282
cho4u4o <cho4u4o@gmail.com>
2025-05-13 11:43:39 +0900

    feat(web): implement timer to signup page

--- apps/web/src/widgets/signup/ui/PhoneCertifyForm.tsx
+++ apps/web/src/widgets/signup/ui/PhoneCertifyForm.tsx
@@ -1,7 +1,13 @@
-import React, { type Dispatch, type SetStateAction, useState } from 'react';
+import React, {
+  type Dispatch,
+  type SetStateAction,
+  useEffect,
+  useState,
+} from 'react';
 
 import { Button, Input } from '@/shared/ui';
 import { useSubmitCode, useSubmitPhoneNumber } from '../api';
+import { cn } from '@festamate/utils';
 
 interface PhoneCertifyFormProps {
   phoneNumber: string;
@@ -12,7 +18,9 @@ export default function PhoneCertifyForm({
   phoneNumber,
   setProcess,
 }: PhoneCertifyFormProps) {
+  const [timeOver, setTimeOver] = useState<boolean>(false);
   const [value, setValue] = useState<string>('');
+  const [count, setCount] = useState<number>(2);
   const { mutate, isPending, isError } = useSubmitCode({
     setProcess,
   });
@@ -28,6 +36,27 @@ export default function PhoneCertifyForm({
     resend({ phoneNumber });
   };
 
+  const timeFormat = (time: number) => {
+    const minute = Math.floor(time / 60);
+    const second = time % 60;
+    const isDigit = time.toString().length === 1;
+    return `${minute}:${isDigit ? `0${second}` : second}`;
+  };
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCount(prev => {
+        if (prev <= 1) {
+          clearInterval(intervalId);
+          setTimeOver(true);
+          return 0;
+        }
+        return prev - 1;
+      });
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div className='flex w-full flex-col gap-6'>
       <div className='flex w-full flex-col gap-2'>
@@ -39,14 +68,23 @@ export default function PhoneCertifyForm({
             disabled
           />
         </div>
-        <div className='flex-1'>
+        <div className='relative flex-1'>
           <Input
             id='phone-number-certification'
             placeholder='인증번호를 입력해 주세요'
             type='number'
             value={value}
             onChange={e => setValue(e.target.value)}
+            disabled={timeOver}
           />
+          <span
+            className={cn(
+              count < 60 ? 'text-important' : 'text-light',
+              'absolute top-2.5 right-4',
+            )}
+          >
+            {timeFormat(count)}
+          </span>
         </div>
       </div>
 
@@ -69,7 +107,11 @@ export default function PhoneCertifyForm({
           onClick={resendClick}
           className='focus:outline-none'
         >
-          <u>인증번호가 오지 않나요? 다시 전송하기</u>
+          <u>
+            {timeOver
+              ? '시간이 초과되었어요. 인증번호를 다시 요청해 주세요.'
+              : '인증번호가 오지 않나요? 다시 전송하기'}
+          </u>
         </button>
       </div>
     </div>
