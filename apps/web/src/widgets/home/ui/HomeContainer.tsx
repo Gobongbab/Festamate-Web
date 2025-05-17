dca3b6bfd1f422e64142fe82679eaf8a13c16ee1
cho4u4o <cho4u4o@gmail.com>
2025-05-17 15:35:12 +0900

    design(web): change text of `BoothInfo` banner

--- apps/web/src/widgets/home/ui/HomeContainer.tsx
+++ apps/web/src/widgets/home/ui/HomeContainer.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useAtomValue } from 'jotai';
+
+import { fetchLoginStatus } from '@festamate/utils';
 
 import { GroupCarousel, GroupList } from '@/shared/ui';
 import { REQUEST } from '@/shared/api';
-import { fetchLoginStatus } from '@festamate/utils';
-import { useAtomValue } from 'jotai';
 import { userAtom } from '@/shared/atom';
+
+import { HiMiniArrowTopRightOnSquare } from 'react-icons/hi2';
 
 export default function HomeContainer() {
   const isLogin = fetchLoginStatus();
@@ -32,7 +35,7 @@ export default function HomeContainer() {
 
 const BoothInfo = () => (
   <button
-    className='rounded-10 p-normal-spacing relative flex h-fit w-full flex-col gap-y-2 bg-cover bg-center text-white focus:outline-none'
+    className='rounded-10 p-normal-spacing relative flex h-fit w-full flex-col gap-y-1 bg-cover bg-center text-white focus:outline-none'
     style={{
       backgroundImage: `url('https://i.pinimg.com/736x/67/a7/a2/67a7a2316cfa18d84af9f03cf11b0919.jpg')`,
     }}
@@ -41,14 +44,15 @@ const BoothInfo = () => (
     }
   >
     <div className='rounded-10 absolute inset-0 bg-black/30' />
-    <div className='z-1 flex items-baseline justify-between'>
+    <div className='z-1 flex items-center justify-between'>
       <span className='agbalumo-regular text-xl font-bold'>Festamate!</span>
-      <span className='hover:text-dark cursor-pointer text-sm text-white'>
-        <u>인스타 바로가기</u>
-      </span>
+      <div className='flex items-center gap-1'>
+        바로가기
+        <HiMiniArrowTopRightOnSquare size={18} className='font-bold' />
+      </div>
     </div>
     <span className='z-1 text-start text-lg font-semibold'>
-      인스타그램에서 다양한 이벤트 중!
+      인스타그램에서 이벤트를 확인해보세요!
     </span>
   </button>
 );
