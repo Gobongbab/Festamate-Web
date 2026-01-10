a3c996cf3a330ffb75cbb2c1b9c1c0bca9ce1426
yummjin <cho4u4o@gmail.com>
2026-01-10 10:13:57 +0900

    chore(*): update base url

--- apps/admin/src/shared/api/user.ts
+++ apps/admin/src/shared/api/user.ts
@@ -13,7 +13,7 @@ interface GetRequestParams<TParams> {
 }
 
 const instance = axios.create({
-  baseURL: 'https://www.pegion.ddns.net/api',
+  baseURL: 'https://pegion.ddns.net/api',
 });
 
 instance.interceptors.request.use(async config => {
